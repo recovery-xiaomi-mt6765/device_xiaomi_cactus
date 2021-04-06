e727d4ba909a4106ad68ad0d10ec82b0a94e1219
Yaroslav Popkov <yarikpopkov1@gmail.com>
2021-04-06 22:24:39 +0500

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,70 +1,52 @@
-#
-# Copyright 2017 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+LOCAL_PATH := device/xiaomi/cactus
 
-# Platform
-TARGET_BOARD_PLATFORM := mt6765
-TARGET_BOOTLOADER_BOARD_NAME := mt6765
-TARGET_NO_BOOTLOADER := true
-
-# Architecture
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a7
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOARD_PLATFORM := mt6765
+TARGET_BOOTLOADER_BOARD_NAME := mt6765
+TARGET_NO_BOOTLOADER := true
 
-# File System
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Kernel
-BOARD_KERNEL_BASE := 0x40000000
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+BOARD_KERNEL_BASE = 0x40000000
+BOARD_KERNEL_OFFSET = 0x00008000
+BOARD_RAMDISK_OFFSET = 0x15000000
+BOARD_TAGS_OFFSET = 0x14000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_RAMDISK_OFFSET := 0x15000000
-BOARD_TAGS_OFFSET := 0x14000000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1 androidboot.selinux=permissive androidboot.configfs=true
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
-TARGET_PREBUILT_KERNEL := device/xiaomi/cactus/prebuilt/kernel
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+BOARD_KERNEL_CMDLINE = bootopt=64S3,32S1,32S1
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_SUPERSU := true
+
+TW_THEME := portrait_hdpi
+
+TW_EXTRA_LANGUAGES := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_MAX_BRIGHTNESS := 255
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_INCLUDE_CRYPTO := true
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_TWRPAPP := true
+TW_SCREEN_BLANK_ON_BOOT := true
+
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 
-# Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-RECOVERY_SDCARD_ON_DATA := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
-# Resolution
-DEVICE_RESOLUTION := 720x1440
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1440
-
-# TWRP
-TARGET_RECOVERY_FSTAB := device/xiaomi/cactus/recovery.fstab
-TW_DEVICE_VERSION := $(shell date -u +"%Y%m%d")
-TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_ALWAYS_RMRF := true
-TW_INCLUDE_CRYPTO := true
-TW_EXCLUDE_SUPERSU := true
-TW_EXCLUDE_TWRPAPP := true
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT :="external_sd"
-TW_MTP_DEVICE := /dev/mtp_usb
-TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.0/file
